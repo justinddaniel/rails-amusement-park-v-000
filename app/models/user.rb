4319706177ea68b5d9150bb581572b5b5e987ec2
Justin Daniel <justinddaniel@gmail.com>
2017-12-08 00:30:47 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+    if self.nausea && self.happiness
+      if self.nausea.to_i > self.happiness.to_i
+        "sad"
+      elsif self.nausea.to_i < self.happiness.to_i
+        "happy"
+      else
+        "neutral"
+      end
+    else
+      "neutral"
+    end
+  end
+
+  def num_of_rides
+    self.attractions.count
+  end
 end
